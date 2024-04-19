f503b17d22480dfa1e497761169e661dcf66414c
= <john2318@naver.com>
2024-04-19 10:17:50 +0900

    two

--- src/f201914095/Main.java
+++ src/f201914095/Main.java
@@ -3,6 +3,7 @@ package f201914095;
 public class Main {
 
   public static void main(String[] args) {
+    Two.print();
     System.out.println("one 201914095");
   }
 }
---
+++ src/f201914095/Two.java
@@ -0,0 +1,8 @@
+package f201514199;
+
+public class Two {
+
+  public static void print() {
+    System.out.println("two");
+  }
+}
